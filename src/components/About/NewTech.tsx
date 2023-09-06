fa8f05539317ce6a2ded90eabb857785a0b2dd06
Szymon Siudzinski <NaktisPL@gmail.com>
2023-09-06 22:14:28 +0200

    Added and configured alias imports

--- src/components/About/NewTech.tsx
+++ src/components/About/NewTech.tsx
@@ -1,15 +1,15 @@
 import styles from "./About.module.css";
-import Bootstrap from "../../assets/logos/Bootstrap.svg";
-import HTML5 from "../../assets/logos/HTML5.svg";
-import React from "../../assets/logos/React.svg";
-import CSS3 from "../../assets/logos/CSS3.svg";
-import express from "../../assets/logos/express.svg";
-import JavaScript from "../../assets/logos/JavaScript.svg";
-import MongoDB from "../../assets/logos/MongoDB.svg";
-import Next from "../../assets/logos/Next.svg";
-import PostgreSQL from "../../assets/logos/PostgreSQL.svg";
-import Tailwind from "../../assets/logos/Tailwind.svg";
-import TypeScript from "../../assets/logos/TypeScript.svg";
+import Bootstrap from "@/assets/logos/Bootstrap.svg";
+import HTML5 from "@/assets/logos/HTML5.svg";
+import React from "@/assets/logos/React.svg";
+import CSS3 from "@/assets/logos/CSS3.svg";
+import express from "@/assets/logos/express.svg";
+import JavaScript from "@/assets/logos/JavaScript.svg";
+import MongoDB from "@/assets/logos/MongoDB.svg";
+import Next from "@/assets/logos/Next.svg";
+import PostgreSQL from "@/assets/logos/PostgreSQL.svg";
+import Tailwind from "@/assets/logos/Tailwind.svg";
+import TypeScript from "@/assets/logos/TypeScript.svg";
 
 export default function Technologies() {
   return (
